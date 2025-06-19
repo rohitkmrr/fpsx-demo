6104a0e889e5758e03d7808ae666d24d2ed4a08f
Priyanka Marpina <43060897+Priyamarpina@users.noreply.github.com>
2025-06-19 15:17:43 +0530

    Update random.js

--- test-plans/playwright/random.js
+++ test-plans/playwright/random.js
@@ -25,7 +25,7 @@ test('Random pass/fail test after Salesforce login', async ({ page }) => {
   await delay(1000); // Post-login buffer
 
   const fiveMins = 300000;
-  await delay(delayInMs);
+  await delay(fiveMins);
   // Generate random number between 1 and 9 for 10s to 90s delay
   const delayMultiplier = Math.floor(Math.random() * 9) + 1;
   const delayInMs = delayMultiplier * 10 * 1000;
