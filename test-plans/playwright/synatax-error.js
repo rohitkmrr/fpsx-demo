f736bfef2937d8d8438cf1f7abb5d8a6e54c5360
Priyanka Marpina <43060897+Priyamarpina@users.noreply.github.com>
2025-08-08 02:35:12 +0530

    Update synatax-error.js

--- test-plans/playwright/synatax-error.js
+++ test-plans/playwright/synatax-error.js
@@ -1,1 +1,13 @@
+import { test } from '@playwright/test';
 
+heyhellooo
+const delay = (ms) => {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+};
+test('Salesforce Recording - Thu May 08 2025 12:59:13 GMT+0530 (India Standard Time)', async ({
+      page,
+}) => {
+      test.setTimeout(0);
+    
+      await delay(1988);;
+});
